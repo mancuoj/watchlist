876756f67088663a1f215930bbd19c6a3fd1744a
mancuoj <1337381768@qq.com>
2022-10-14 14:43:23 +0800

    feat: format files

--- app.py
+++ app.py
@@ -3,19 +3,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # 虚拟数据
-name = 'Mancuoj'
+name = "Mancuoj"
 movies = [
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
+    {"title": "A Perfect World", "year": "1993"},
+    {"title": "Leon", "year": "1994"},
+    {"title": "Mahjong", "year": "1996"},
+    {"title": "Swallowtail Butterfly", "year": "1996"},
+    {"title": "King of Comedy", "year": "1999"},
+    {"title": "Devils on the Doorstep", "year": "1999"},
+    {"title": "WALL-E", "year": "2008"},
+    {"title": "The Pork of Music", "year": "2012"},
 ]
+
 
 @app.route("/")
 def index():
     # 渲染模板，传入变量
-    return render_template('index.html', name=name, movies=movies)
+    return render_template("index.html", name=name, movies=movies)
